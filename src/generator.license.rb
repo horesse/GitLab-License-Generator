051358e8a0cba6c12159d41bd1aa7a2b25427789
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2024-06-15 00:27:41 +0300

    заменил инфу о компании

--- src/generator.license.rb
+++ src/generator.license.rb
@@ -74,9 +74,9 @@ license = Gitlab::License.new
 
 # don't use gitlab inc, search `gl_team_license` in lib for details
 license.licensee = {
-  "Name"    => "Tim Cook",
-  "Company" => "Apple Computer, Inc.",
-  "Email"   => "tcook@apple.com"
+  "Name"    => "Horesse",
+  "Company" => "Moodschool",
+  "Email"   => "admin@moodschool.ru"
 }
 
 # required of course
